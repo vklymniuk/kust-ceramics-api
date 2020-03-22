3f41fcd0f4dbbe8fc76228e4dc2e93bdf0705eb4
Vladimir Klymniuk <vladimir.klymniuk@gmail.com>
2020-03-22 23:00:30 +0200

    Change naming of database connection variables.

--- config/index.js
+++ config/index.js
@@ -18,13 +18,13 @@ const index = convict({
         env: 'DEBUG',
     },
     firebase:  {
-        authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-        apiKey: process.env.FIREBASE_API_KEY,
-        databaseURL: process.env.FIREBASE_DATABASE_URL,
-        projectId: process.env.FIREBASE_PROJECT_ID,
-        storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-        messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-        appId: process.env.FIREBASE_APP_ID
+        authDomain: process.env._FIREBASE_AUTH_DOMAIN,
+        apiKey: process.env._FIREBASE_API_KEY,
+        databaseURL: process.env._FIREBASE_DATABASE_URL,
+        projectId: process.env._FIREBASE_PROJECT_ID,
+        storageBucket: process.env._FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env._FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env._FIREBASE_APP_ID
     },
     rateLimit: {
         windowMs: {
